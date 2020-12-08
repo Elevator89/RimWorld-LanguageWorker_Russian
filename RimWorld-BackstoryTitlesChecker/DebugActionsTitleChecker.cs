afc637564af7363bb5da128ea55c40adc0b3ca48
Anrey lemin <anlemin@mail.ru>
2020-12-08 13:22:10 +0300

    Add backstory titles' length checker mod

---
+++ RimWorld-BackstoryTitlesChecker/DebugActionsTitleChecker.cs
@@ -0,0 +1,42 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using UnityEngine;
+
+namespace BackstoryTitlesChecker
+{
+	public static class DebugActionsTitleChecker
+	{
+		[DebugAction("Translation", null, allowedGameStates = AllowedGameStates.Entry)]
+		private static void CheckTitleLengths()
+		{
+			Backstory[] backstories = BackstoryDatabase.allBackstories.Values.ToArray();
+
+			Text.Font = GameFont.Small;
+			Text.Anchor = TextAnchor.MiddleLeft;
+
+			float maxWidth = 160f;
+
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine($"Oversized backstory titles (>{maxWidth}px):");
+
+			foreach (Backstory backstory in backstories)
+			{
+				CheckAndLogSize(backstory.identifier + ".title", backstory.title.CapitalizeFirst());
+				CheckAndLogSize(backstory.identifier + ".titleFemale", backstory.titleFemale.CapitalizeFirst());
+			}
+
+			Log.Message(sb.ToString());
+
+			void CheckAndLogSize(string id, string title)
+			{
+				Vector2 size = Text.CalcSize(title);
+				if (size.x > maxWidth)
+					sb.AppendLine($"{id}: {title} - {size.x}px");
+			}
+		}
+	}
+}
